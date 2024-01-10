e1525040ad0d95ef12030a2a5e7ce9ded6754511
rd273001 <rd273001@gmail.com>
2024-01-11 03:50:09 +0530

    Initial Commit - Created React Native app

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView style={ { flex: 1, backgroundColor: '#F5F5F5'} }>
+    </SafeAreaView>
+  );
+};
+
+export default App;
